035c764c29b66e8f85bd7f275476539ecb78544b
Shelby Hendrickx <shelby.hendrickx@hotmail.com>
2019-04-26 03:26:41 +0200

    added split

---
+++ src/commands/splitcmd.cpp
@@ -0,0 +1,21 @@
+/* author: Shelby Hendrickx */
+#include <iostream>
+#include "../../include/commands/commandfactory.h"
+#include "../../include/commands/splitcmd.h"
+#include "../../include/sstring.h"
+#include "../../include/textlist.h"
+#include "../../include/textutils.h"
+
+SplitCommand::SplitCommand()
+{}
+
+int SplitCommand::execute(TextList& text, const SString& cmd)
+{
+    size_t line = TextUtils::getLineNumberFromCommand(text, cmd);
+    if (line == -1) return -1;
+
+    SString splitWord = TextUtils::getLineFromCin();
+
+    text.splitLine(splitWord, line);
+    return 0;
+}
